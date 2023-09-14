b455fc66882977ebf1411613b4f68108f72a8ff7
cameron-leung <cameronleung@berkeley.edu>
2023-09-13 17:19:12 -0700

    Update AList.java

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -6,30 +6,39 @@ package lec7_lists4.DIY;
  */
 public class AList {
     /** Creates an empty list. */
+    private int size;
+    private int[] items;
     public AList() {
+        items = new int[100];
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        items[size] = x;
+        size += 1;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return items[size-1];        
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return items[i];        
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;        
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int x = item[size];
+        item[size] = null;
+        size -= 1;
+        return x;
     }
 } 
